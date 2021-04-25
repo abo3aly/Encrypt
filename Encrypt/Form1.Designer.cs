d6221d6123c5897b3b9da6ff35d045ca7e45e979
meshaal <aboremas1516@gmail.com>
2021-04-26 00:11:31 +0300

    Add project files.

---
+++ Encrypt/Form1.Designer.cs
@@ -0,0 +1,156 @@
+﻿
+namespace Encrypt
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_Encrypt = new System.Windows.Forms.Button();
+            this.txt_Decrypt = new System.Windows.Forms.TextBox();
+            this.txt_Encrypt = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.btn_Decrypt = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_Encrypt
+            // 
+            this.btn_Encrypt.Location = new System.Drawing.Point(251, 255);
+            this.btn_Encrypt.Name = "btn_Encrypt";
+            this.btn_Encrypt.Size = new System.Drawing.Size(112, 36);
+            this.btn_Encrypt.TabIndex = 0;
+            this.btn_Encrypt.Text = "التشفير ";
+            this.btn_Encrypt.UseVisualStyleBackColor = true;
+            this.btn_Encrypt.Click += new System.EventHandler(this.btn_Encrypt_Click);
+            // 
+            // txt_Decrypt
+            // 
+            this.txt_Decrypt.Location = new System.Drawing.Point(87, 22);
+            this.txt_Decrypt.Multiline = true;
+            this.txt_Decrypt.Name = "txt_Decrypt";
+            this.txt_Decrypt.Size = new System.Drawing.Size(276, 98);
+            this.txt_Decrypt.TabIndex = 1;
+            // 
+            // txt_Encrypt
+            // 
+            this.txt_Encrypt.Location = new System.Drawing.Point(87, 140);
+            this.txt_Encrypt.Multiline = true;
+            this.txt_Encrypt.Name = "txt_Encrypt";
+            this.txt_Encrypt.Size = new System.Drawing.Size(276, 98);
+            this.txt_Encrypt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(369, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(92, 25);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "رموز التشفير : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(371, 179);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(90, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "المراد تشفيره :";
+            // 
+            // btn_Decrypt
+            // 
+            this.btn_Decrypt.Location = new System.Drawing.Point(87, 255);
+            this.btn_Decrypt.Name = "btn_Decrypt";
+            this.btn_Decrypt.Size = new System.Drawing.Size(112, 36);
+            this.btn_Decrypt.TabIndex = 5;
+            this.btn_Decrypt.Text = "فك التشفير ";
+            this.btn_Decrypt.UseVisualStyleBackColor = true;
+            this.btn_Decrypt.Click += new System.EventHandler(this.btn_Decrypt_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.ForeColor = System.Drawing.Color.Maroon;
+            this.label3.Location = new System.Drawing.Point(12, 318);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(91, 25);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "ربي زدني علما ";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.ForeColor = System.Drawing.Color.Maroon;
+            this.label4.Location = new System.Drawing.Point(369, 318);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(90, 25);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "مشعل العنزي";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 25F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.ClientSize = new System.Drawing.Size(476, 380);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_Decrypt);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_Encrypt);
+            this.Controls.Add(this.txt_Decrypt);
+            this.Controls.Add(this.btn_Encrypt);
+            this.Font = new System.Drawing.Font("Sakkal Majalla", 15F, System.Drawing.FontStyle.Bold);
+            this.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.ShowIcon = false;
+            this.ShowInTaskbar = false;
+            this.Text = "التشفير";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_Encrypt;
+        private System.Windows.Forms.TextBox txt_Decrypt;
+        private System.Windows.Forms.TextBox txt_Encrypt;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button btn_Decrypt;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Encrypt/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace Encrypt
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        public static string Encrypt(string password)
+        {
+            char[] pass = password.ToCharArray();
+            string[] Sysb = { "*", "#", "&", "@", "%" };
+            string NewPass = "";
+            Random rm = new Random();
+            for (int i = pass.Length - 1; i >= 0; i--)
+            {
+                NewPass += ((int)pass[i]).ToString() + Sysb[rm.Next(0, 5)];
+            }
+
+            return NewPass;
+        }
+        public static string Decrypt(string password)
+        {
+            string[] tokens = password.Split('*', '#', '&', '@', '%');
+            string pass = "";
+
+            for (int i = tokens.Length - 2; i >= 0; i--)
+            {
+                pass += ((char)Convert.ToInt32(tokens[i])).ToString();
+
+            }
+            return pass;
+
+        }
+
+        private void btn_Encrypt_Click(object sender, EventArgs e)
+        {
+
+            if (txt_Encrypt.Text == "")
+            {
+                MessageBox.Show("ادخل المراد تشفيره");
+                txt_Encrypt.Focus();
+                return;
+            }
+            else
+            {
+                txt_Decrypt.Text = Encrypt(txt_Encrypt.Text);
+
+            }
+        }
+
+        private void btn_Decrypt_Click(object sender, EventArgs e)
+        {
+            if (txt_Decrypt.Text == ""){
+                MessageBox.Show("ادخل رموز التشفير");
+                txt_Decrypt.Focus();
+                return;
+            }
+            else
+            {
+                txt_Encrypt.Text = Decrypt(txt_Decrypt.Text);
+            }
+
+            
+            
+        }
+    }
+}
